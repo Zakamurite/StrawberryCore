480c75831982c99f59eb15c11aea6120ad9d5006
Yaki Khadafi <elsoldollo@gmail.com>
2012-06-24 16:54:11 +0300

    it was ok
    Signed-off-by: Yaki Khadafi <elsoldollo@gmail.com>

--- src/game/Player.cpp
+++ src/game/Player.cpp
@@ -2844,8 +2844,8 @@ void Player::SendInitialSpells()
 
         data << uint32(itr->first);
 
-        data << uint16(itr->second.itemid);                 // cast item id
-        data << uint32(sEntry->GetCategory());              // spell category
+        data << uint32(itr->second.itemid);                 // cast item id
+        data << uint16(sEntry->GetCategory());              // spell category
 
         // send infinity cooldown in special format
         if(itr->second.end >= infTime)
